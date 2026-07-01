f94649ef00daee2598bfe91ce1b4a6637f1a79a0
Jari O. O. Pennanen <ciantic@oksidi.com>
2026-07-02 00:58:57 +0300

    Use tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  entry: "src/index.ts",
+  dts: true,
+  unbundle: true,
+  clean: true,
+  format: ["esm"],
+  outDir: "dist",
+});
